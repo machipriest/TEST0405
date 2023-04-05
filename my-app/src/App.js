51611d36e1d97aa28f0d148d1394b6f8364c3884
Max Gu <3253qqz@gmail.com>
2023-04-05 15:06:20 +0800

    首頁更改

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,152 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div id="popup5_2">
+        <div id="contentArea2" >
+            <div id="contentFrame2" onclick="updateScroll()">
+                <div class="contentGroupFrame">
+                    <div class="contentTimeFrame">
+                        PM 06:25 ▶
+                    </div>
+                    <div class="contentTextFrame">
+                        <p>這次全班考試的綜合成績令我感到有點<span class="markText" style={{"background-color":"rgb(234, 86, 86)"}}>失望</span></p> 
+                    </div>
+                </div>
+                <hr class="hr-style"/>
+                <div class="contentGroupFrame">
+                    <div class="contentTimeFrame">
+                        AM 08:30 ▶
+                    </div>
+                    <div class="contentTextFrame">
+                        <p>今天把任務都完成的話就可以<span class="markText" style={{"background-color":"red"}}>看電視</span></p>
+                    </div>
+                </div>
+                <hr class="hr-style"/>
+                <div class="contentGroupFrame">
+                    <div class="contentTimeFrame">
+                        AM 08:31 ▶
+                    </div>
+                    <div class="contentTextFrame">
+                        <p>我很不擅長把心情說出來，對我而言闡述心情是件<span class="markText" style={{"background-color":"rgb(250, 178, 178)"}}>害羞</span>的事</p> 
+                    </div>
+                </div>
+                <hr class="hr-style"/>
+                <div class="contentGroupFrame">
+                    <div class="contentTimeFrame">
+                        AM 10:45 ▶
+                    </div>
+                    <div class="contentTextFrame">
+                        <p>這個故事<span class="markText" style={{"background-color":"rgb(75, 235, 73)"}}>甜蜜</span>中帶點<span class="markText" style={{"background-color":"rgb(234, 86, 86)"}}>悲情</span></p>
+                    </div>
+                </div>
+                <hr class="hr-style"/>
+                <div class="waitFrame">
+                    等待中...
+                </div>
+            </div>
+        </div>
+        <div id="informationNav2">
+            <div class="informationFrame2" id="information_green5">
+                <div class="greenIcon2">
+
+                </div>
+                <div class="vocabularyFrame">
+                    <div class="vocabulary_green2_2">
+                        <p class="">喜出望外</p>
+                    </div>
+                    <div class="vocabulary_green2_2">
+                        <p class="">快樂</p>
+                    </div>
+                    <div class="vocabulary_green2_2">
+                        <p class="">喜愛</p>
+                    </div>
+                    <div class="vocabulary_green2_2">
+                        <p class="">豁朗</p>
+                    </div>
+                    <div class="vocabulary_green2_2">
+                        <p class="">入迷</p>
+                    </div>
+                    <div class="vocabulary_green2_2">
+                        <p class="">快活</p>
+                    </div>
+                    <div class="vocabulary_green2_2">
+                        <p class="">甜蜜</p>
+                    </div>
+                    <div class="vocabulary_green2">
+                        <p class="">看電視</p>
+                    </div>
+                    <div class="vocabulary_green2">
+                        <p class="">理性</p>
+                    </div>
+                </div>
+            </div>
+            <div class="informationFrame2" id="information_red5">
+                <div class="redIcon2">
+
+                </div>
+                <div class="vocabularyFrame">
+                    <div class="vocabulary_red2_2">
+                        <p class="">沉痛</p>
+                    </div>
+                    <div class="vocabulary_red2_2">
+                        <p class="">悲傷</p>
+                    </div>
+                    <div class="vocabulary_red2_2">
+                        <p class="">絕望</p>
+                    </div>
+                    <div class="vocabulary_red2_2">
+                        <p class="">厭煩</p>
+                    </div>
+                    <div class="vocabulary_red2_2">
+                        <p class="">震怒</p>
+                    </div>
+                    <div class="vocabulary_red2_2">
+                        <p class="">鬱悶</p>
+                    </div>
+                    <div class="vocabulary_red2_2">
+                        <p class="">嫉妒</p>
+                    </div>
+                    <div class="vocabulary_red2_2">
+                        <p class="">落寞</p>
+                    </div>
+                    <div class="vocabulary_red2_2">
+                        <p class="">心急如焚</p>
+                    </div>
+                    <div class="vocabulary_red2_2">
+                        <p class="">痛苦</p>
+                    </div>
+                    <div class="vocabulary_red2_2">
+                        <p class="">失敗</p>
+                    </div>
+                    <div class="vocabulary_red2_2">
+                        <p class="">失望</p>
+                    </div>
+                    <div class="vocabulary_red2_2">
+                        <p class="">悲情</p>
+                    </div>
+                    <div class="vocabulary_red2">
+                        <p class="">忐忑不安</p>
+                    </div>
+                    <div class="vocabulary_red2">
+                        <p class="">掛念</p>
+                    </div>
+                    <div class="vocabulary_red2">
+                        <p class="">害羞</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="functionArea">
+            <div id="stopBtn">
+                STOP
+            </div>
+        </div>
     </div>
+    </>
   );
 }
 
-export default App;
+  export default App;
